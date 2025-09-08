8be1ffb413dbdff02b5564e046a459823ba2c84f
Joryn Vincent S. Arabilla <jorynvincentarabilla@gmail.com>
2025-09-08 23:17:48 +0800

    Delete Exer1_Declarative.java

--- Exer1_Declarative.java
+++
@@ -1,9 +0,0 @@
-public class Exer1_Declarative {
-    public static void main(String[] args) {
-        int number = 7;
-
-        String result = (number % 2 == 0) ? "Even" : "Odd";
-
-        System.out.println("Number " + number + " is " + result + " (Declarative).");
-    }
-}
